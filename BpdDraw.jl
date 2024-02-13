9dd6706d23482007f104373d7c400c9921a4ccc6
Dave Anderson <62109185+pseudoeffective@users.noreply.github.com>
2024-02-13 14:53:25 -0500

    Add files via upload

--- BpdDraw.jl
+++ BpdDraw.jl
@@ -4,6 +4,20 @@
 using Plots
 
 include("./BpdBase.jl")
+include("./Drifts.jl")
+
+
+# updated to use integer matrices
+# "O" => 0
+# "+" => 1
+# "/" => 2
+# "%" => 3
+# "|" => 4
+# "-" => 5
+# "." => 6
+# "*" => 7
+# "" => 8
+# "o" => 9
 
 
 function draw_se_elbow_curve(x1, y1, x3, y3)
@@ -23,27 +37,30 @@ function draw_nw_elbow_curve(x1, y1, x3, y3)
 end
 
 
+
+# TO DO: get (faint) grid to appear
+
 function draw_bpd(B,fn; img_size=(300,300))
-    n, m = size(B)
+    n, m = size(B.m)
     p = plot(; xlim=(0, n), ylim=(0, m), aspect_ratio=:equal, legend=false, grid=false, framestyle=:none, tick_direction=:none, size=img_size)
     
     for i = 1:n
         for j = 1:n
             y, x = n-i, j-1  # Transpose and invert the y-coordinate
-            tile = B[i, j]
-            if tile == "O"
+            tile = B.m[i, j]
+            if tile == 0
                 plot!([x, x+1, x+1, x, x], [y, y, y+1, y+1, y], linecolor=:orange, linewidth=0.5, seriestype=:shape, fillalpha=0)
-            elseif tile == "+"
+            elseif tile == 1
                 plot!([x+0.5, x+0.5], [y, y+1], linecolor=:blue, linewidth=2)
                 plot!([x, x+1], [y+0.5, y+0.5], linecolor=:blue, linewidth=2)
-            elseif tile == "|"
+            elseif tile == 4
                 plot!([x+0.5, x+0.5], [y, y+1], linecolor=:blue, linewidth=2)
-            elseif tile == "-"
+            elseif tile == 5
                 plot!([x, x+1], [y+0.5, y+0.5], linecolor=:blue, linewidth=2)
-            elseif tile == "/"
+            elseif tile == 2
                 x_vals, y_vals = draw_se_elbow_curve(x+1, y+0.5, x+0.5, y)
                 plot!(x_vals, y_vals, linecolor=:blue, linewidth=2)
-            elseif tile == "%"
+            elseif tile == 3
                 x_vals, y_vals = draw_nw_elbow_curve(x+0.5, y+1, x, y+0.5)
                 plot!(x_vals, y_vals, linecolor=:blue, linewidth=2)
             end
@@ -57,7 +74,7 @@ end
 
 function print_flat_bpds(w,fn,fmt)
 
-  local ds=flat_bpds(w)
+  local ds=collect( flat_bpds(w) )
   local n=length(ds)
 
   for i=1:n
@@ -66,4 +83,45 @@ function print_flat_bpds(w,fn,fmt)
 
 end
 
+
+
+# TO DO: get (faint) grid to appear
+
+function draw_drift(B,fn; img_size=(300,300))
+    n, m = size(B.m)
+    p = plot(; xlim=(0, n), ylim=(0, m), aspect_ratio=:equal, legend=false, tick_direction=:none, size=img_size)
+    
+    for i = 1:n
+        for j = 1:n
+            y, x = n-i, j-1  # Transpose and invert the y-coordinate
+            tile = B.m[i, j]
+
+            if tile == 0
+                plot!([x, x+1, x+1, x, x], [y, y, y+1, y+1, y], linecolor=:orange, linewidth=2, seriestype=:shape, fillalpha=0)
+
+            elseif isa(tile, Tuple)
+                plot!([x, x+1, x+1, x, x], [y, y, y+1, y+1, y], linecolor=:orange, linewidth=2, seriestype=:shape, fillalpha=0)
+                if tile[2]
+                    annotate!([(x + 0.5, y + 0.5, text(string(tile[1]), :center, 10, :red))])
+                else
+                     annotate!([(x + 0.5, y + 0.5, text(string(tile[1]), :center, 10))])
+                end
+
+            elseif tile == 1
+                plot!([x+0.5, x+0.5], [y+0.1, y+0.9], linecolor=:blue, linewidth=1)
+                plot!([x+0.1, x+0.9], [y+0.5, y+0.5], linecolor=:blue, linewidth=1)
+
+            elseif tile == 6 || tile == 7
+              scatter!([(x + 0.5)], [(y + 0.5)], markercolor=:blue, markersize=2, markerstrokewidth=0)
+            end
+
+            if tile == 7
+                plot!([x, x+1, x+1, x, x], [y, y, y+1, y+1, y], linecolor=:orange, linewidth=2, seriestype=:shape, fillalpha=0)
+
+            end
+        end
+    end
+    plot!(; framestyle=:box, linecolor=:black, linewidth=3, ticks=nothing, grid=true, gridcolor=:lightgrey, gridalpha=0.5, gridlinewidth=0.5)
+    savefig(fn)
+end
 
